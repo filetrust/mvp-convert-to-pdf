56ff573603e2c5a6b6d1b4409ee743633b9bbaf4
Abhi <anish3009@gmail.com>
2020-06-16 19:27:05 +0530

    Server modifications

--- backend/express/src/pdf-engine/engine.js
+++ backend/express/src/pdf-engine/engine.js
@@ -1,5 +1,6 @@
 const { writeFileSync }                             = require("fs");
 const path                                          = require("path");
+const { execSync }                                  = require("child_process");
 const convertCommand                                = 'export HOME=/root && ./instdir/program/soffice.bin --headless --norestore --invisible --nodefault --nofirststartwizard --nolockcheck --nologo --convert-to "pdf:writer_pdf_Export" --outdir /tmp';
 
 const MAX_FILE_SIZE                                 = 30 * 1024 * 1024;
