e08ba776baddc56bbbc7c29ffb2795e712de513b
anishagarwal <anish3009@gmail.com>
2020-07-05 17:32:48 +0530

    Moved image converter here

--- backend/express/src/pdf-engine/engine.js
+++ backend/express/src/pdf-engine/engine.js
@@ -4,6 +4,7 @@ const path                                          = require("path");
 const { execSync }                                  = require("child_process");
 const libre                                         = require('libreoffice-convert');
 const REGEX_SAFE_FILE_NAME                          = /[^a-zA-Z0-9-_\.]/g
+const PDF2Pic   = require("pdf2pic");
 
 const MAX_FILE_SIZE                                 = 30 * 1024 * 1024;
 
@@ -14,6 +15,34 @@ async function convert(request, response){
     filename_sanitized                                = sanitize_file_name(filename);
     var pdfFileURLProm                                = convertFileToPDF(base64File, filename_sanitized,response);
 }
+
+async function convert_to_img(request, response){
+    console.log('Request Body - '+JSON.stringify(request.body))
+    const filename                                    = request.body.filename;
+    const base64File                                  = request.body.base64File;
+    filename_sanitized                                = sanitize_file_name(filename);
+    const fileBuffer                                  = new Buffer(base64File, "base64");
+    const fileError                                   = validate(fileBuffer);
+    if (fileError) {
+        response.status(400).download('File too big or too small')
+        return;
+    }
+    writeFileSync(`/tmp/${filename_sanitized}`, fileBuffer);
+    const pdf2pic = new PDF2Pic({
+      density       : 100,                  // output pixels per inch
+      savename      : filename_sanitized,   // output file name
+      savedir       : '/tmp',               // output file location
+      format        : "png",                // output file format
+      size          : "600x600"             // output size in pixels
+    });
+    pdf2pic.convertBulk('/tmp/'+"/"+filename_sanitized, -1).then((resolve) => {
+      console.log("Image converted successfully!");
+      const imgBytes = readFileSync('/tmp'+filename_sanitized+'.png')
+      response.status(200).send({'filename' : filename_sanitized+'.png' , "base64file" : Buffer.from(imgBytes).toString('base64')})
+      return resolve;
+    });
+}
+
 
 function convertFileToPDF(base64File,filename, response) {
     const fileBuffer                                  = new Buffer(base64File, "base64");
@@ -71,5 +100,6 @@ function sanitize_file_name(file_name) {
 }
 
 module.exports = {
-    convert
+    convert,
+    convert_to_img
 }
